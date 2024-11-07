b5ff79ff4f9646a8283ccac93dda0f9686e8d022
Dancilenina <dada@gmail.com>
2024-11-07 15:16:05 +0000

    thirst commit

---
+++ index.js
@@ -0,0 +1,76 @@
+let monAge = 30
+console.log(monAge)
+const  nomPrenom ="Dancile"
+console.log(nomPrenom)
+monAge=43
+console.log(monAge)
+const entreePremieurSemaine =1000
+const entreeDeuxiemeSemaine =2000
+const entreeTotal = entreePremieurSemaine+entreeDeuxiemeSemaine
+console.log(entreeTotal)
+const NoteHabimanaCedrick =14
+const NotebayubaheCharite =17
+const NoteIrambonaOdilo=20
+const NoteTotal= NoteHabimanaCedrick+NotebayubaheCharite+NoteIrambonaOdilo
+console.log(NoteTotal)
+let mangue =200
+mangue+=10
+console.log(mangue)
+let fruitAvocat=270
+  fruitAvocat+=20
+  console.log(fruitAvocat) 
+  let premierePartie="mon nom"
+  let deuxiemePartie=",prenom."
+  let punchline=premierePartie+deuxiemePartie
+console.log(punchline)
+let punchlin="mon,prenom"
+punchline+=",date."
+const monPersonage = {
+  nom : "ininahazwe",
+  prenom : "dancile",
+age : 23,
+nationalite : "Burundaise"
+
+
+}
+
+
+console.log(monPersonage)
+
+const  lesAutiersOrdinateur = {
+  clavier: "portable",
+  souris : "portabl",
+  ecran : "ordin",
+  imprimenter : "ordina",
+
+
+
+
+}
+
+console.log(lesAutiersOrdinateur)
+
+const ticket = {
+  nomFilm : "indundi",
+  prix:200,
+  numeroSalle:3
+}
+let nom="Dancil"
+let texteAffichage = "Bonjour " + nom + ", votre film " + ticket.nomFilm + " est en salle " +ticket.numeroSalle
+
+console.log(texteAffichage)
+
+
+
+
+   
+
+
+
+
+
+ 
+
+
+
+
